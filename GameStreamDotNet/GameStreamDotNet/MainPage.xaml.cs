f249fb14168a15aea591bf62f435b2f981ad8061
Vijay Prakash <vijay.prakash@microsoft.com>
2018-08-13 16:51:33 -0700

    Added calls to send a challenge request and response and verify the server signature.

--- GameStreamDotNet/GameStreamDotNet/MainPage.xaml.cs
+++ GameStreamDotNet/GameStreamDotNet/MainPage.xaml.cs
@@ -12,6 +12,7 @@
     using Org.BouncyCastle.Asn1.X509;
     using Org.BouncyCastle.X509;
     using Org.BouncyCastle.Math;
+    using Org.BouncyCastle.Crypto.Digests;
     using Org.BouncyCastle.Crypto.Generators;
     using Org.BouncyCastle.Crypto.Operators;
     using Org.BouncyCastle.OpenSsl;
@@ -39,6 +40,11 @@
 
         private async void PairButton_Click(object sender, RoutedEventArgs e)
         {
+            // Create SHA256 hash digest. This is not supported by server version < 7
+            // (need to use SHA1 for those cases) but that doesn't really matter right now.
+            IDigest hashAlgorithm = new Sha256Digest();
+            int hashDigestSize = hashAlgorithm.GetDigestSize();
+
             // Create and salt pin
             byte[] salt = this.GenerateRandomBytes(16);
             string pin = GenerateRandomPin();
@@ -100,15 +106,17 @@
             byte[] pemCertBytes = Encoding.UTF8.GetBytes(certString);
             byte[] uniqueId = GenerateRandomBytes(8);
 
-            // Get server certificate
+            // Get server certificate.
+            // TODO: Call should have no timeout because it requires the user to enter a pin.
             PairResponse pairResponse = null;
             using (HttpClient httpClient = new HttpClient())
             {
                 string uriString =
                     string.Format(
-                        "http://{0}:47989/pair?uniqueid={1}&devicename=roth&updateState=1&phrase=getservercert&salt={2}&clientcert={3}",
+                        "http://{0}:47989/pair?uniqueid={1}&uuid={2}&devicename=roth&updateState=1&phrase=getservercert&salt={3}&clientcert={4}",
                         ipAddressTextBox.Text,
                         BytesToHex(uniqueId),
+                        Guid.NewGuid(),
                         BytesToHex(salt),
                         BytesToHex(pemCertBytes));
                 using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, new Uri(uriString)))
@@ -140,8 +148,108 @@
             byte[] serverCertBytes = HexToBytes(pairResponse.PlainCert);
             X509Certificate serverCertificate = new X509CertificateParser().ReadCertificate(serverCertBytes);
 
-            // Generate a random challenge and encrypt it using AES
+            // Hash the salt and pin and use it to generate an AES key. 
+            byte[] hashedSaltAndPin = HashData(hashAlgorithm, saltAndPin);
+            ICipherParameters aesKey = GenerateCipherKey(hashedSaltAndPin);
+
+            // Generate a random challenge and encrypt it using AES.
             byte[] challenge = GenerateRandomBytes(16);
+            byte[] encryptedChallenge = DoAesCipher(true, aesKey, challenge);
+
+            // Send the encrypted challenge to the server.
+            // TODO: Call should have a timeout.
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string uriString =
+                    string.Format(
+                        "http://{0}:47989/pair?uniqueid={1}&uuid={2}&devicename=roth&updateState=1&clientchallenge={3}",
+                        ipAddressTextBox.Text,
+                        BytesToHex(uniqueId),
+                        Guid.NewGuid(),
+                        BytesToHex(encryptedChallenge));
+                using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, new Uri(uriString)))
+                {
+                    using (HttpResponseMessage response = await httpClient.SendRequestAsync(request))
+                    {
+                        outputTextBox.Text = $"Send challenge status code: {response.StatusCode}\n";
+                        string responseContent = await response.Content.ReadAsStringAsync();
+
+                        outputTextBox.Text += responseContent + "\n";
+                        XmlSerializer serializer = new XmlSerializer(typeof(PairResponse));
+                        pairResponse = serializer.Deserialize((await response.Content.ReadAsInputStreamAsync()).AsStreamForRead()) as PairResponse;
+                    }
+                }
+            }
+
+            if (pairResponse == null || !pairResponse.Paired)
+            {
+                outputTextBox.Text += "Pairing failed.\n";
+                return;
+            }
+
+            // Decode the server's response and subsequent challenge.
+            byte[] encryptedServerChallengeResponse = HexToBytes(pairResponse.ChallengeResponse);
+            byte[] decryptedServerChallengeResponse = DoAesCipher(false, aesKey, encryptedServerChallengeResponse);
+
+            byte[] serverResponse = new byte[hashDigestSize];
+            byte[] serverChallenge = new byte[hashDigestSize];
+            Array.Copy(decryptedServerChallengeResponse, serverResponse, hashDigestSize);
+            Array.Copy(decryptedServerChallengeResponse, hashDigestSize, serverChallenge, 0, hashDigestSize);
+
+            // Using another 16 byte secret, compute a challenge response hash using the secret, 
+            // our certificate signature, and the challenge.
+            byte[] clientSecret = GenerateRandomBytes(16);
+            byte[] challengeResponseHash = 
+                HashData(
+                    hashAlgorithm, 
+                    ConcatenateByteArrays(serverChallenge, certificate.GetSignature(), clientSecret));
+            byte[] encryptedChallengeResponse = DoAesCipher(true, aesKey, challengeResponseHash);
+
+            // Send the challenge response to the server.
+            // TODO: Call should have a timeout.
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string uriString =
+                    string.Format(
+                        "http://{0}:47989/pair?uniqueid={1}&uuid={2}&devicename=roth&updateState=1&serverchallengeresp={3}",
+                        ipAddressTextBox.Text,
+                        BytesToHex(uniqueId),
+                        Guid.NewGuid(),
+                        BytesToHex(encryptedChallengeResponse));
+                using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, new Uri(uriString)))
+                {
+                    using (HttpResponseMessage response = await httpClient.SendRequestAsync(request))
+                    {
+                        outputTextBox.Text = $"Send challenge response status code: {response.StatusCode}\n";
+                        string responseContent = await response.Content.ReadAsStringAsync();
+
+                        outputTextBox.Text += responseContent + "\n";
+                        XmlSerializer serializer = new XmlSerializer(typeof(PairResponse));
+                        pairResponse = serializer.Deserialize((await response.Content.ReadAsInputStreamAsync()).AsStreamForRead()) as PairResponse;
+                    }
+                }
+            }
+
+            if (pairResponse == null || !pairResponse.Paired)
+            {
+                outputTextBox.Text += "Pairing failed.\n";
+                // TODO: Unpair here by calling http://<blah>/unpair?uniqueid={1}&uuid={2}.
+                return;
+            }
+
+            // Get the server's signed secret.
+            byte[] serverSecretResponse = HexToBytes(pairResponse.PairingSecret);
+            byte[] serverSecret = new byte[16];
+            byte[] serverSignature = new byte[256];
+            Array.Copy(serverSecretResponse, 0, serverSecret, 0, serverSecret.Length);
+            Array.Copy(serverSecretResponse, serverSecret.Length, serverSignature, 0, serverSignature.Length);
+
+            if (!VerifySIgnature(serverSecret, serverSignature, serverCertificate))
+            {
+                outputTextBox.Text += "Pairing failed.\n";
+                // TODO: Unpair as above.
+                return;
+            }
         }
 
         private static string GenerateRandomPin()
@@ -183,20 +291,37 @@
             return byteValue;
         }
 
-        private static byte[] HashData(string hashAlgorithmName, byte[] data)
+        private static byte[] ConcatenateByteArrays(params byte[][] byteArrays)
         {
-            HashAlgorithmProvider hashAlgorithm = HashAlgorithmProvider.OpenAlgorithm(hashAlgorithmName);
+            if (byteArrays.Length < 2)
+            {
+                throw new ArgumentException("Cannot concatenate less than two arrays.", nameof(byteArrays));
+            }
 
-            return hashAlgorithm.HashData(data.AsBuffer()).ToArray();
+            int totalLength = 0;
+            for (int i = 0; i < byteArrays.Length; i++)
+            {
+                totalLength += byteArrays[i].Length;
+            }
+
+            byte[] result = new byte[totalLength];
+            int position = 0;
+            for (int i = 0; i < byteArrays.Length; i++)
+            {
+                Array.Copy(byteArrays[i], 0, result, position, byteArrays[i].Length);
+                position += byteArrays[i].Length;
+            }
+
+            return result;
         }
 
-        private static CryptographicKey GenerateAesKey(string hashAlgorithmName, byte[] data)
+        private static byte[] HashData(IDigest hashAlgorithm, byte[] data)
         {
-            SymmetricKeyAlgorithmProvider symmetricKeyAlgorithm =
-                SymmetricKeyAlgorithmProvider.OpenAlgorithm(SymmetricAlgorithmNames.AesEcb);
-            IBuffer keyMaterial = HashData(hashAlgorithmName, data).AsBuffer(0, 16);
+            byte[] hashedData = new byte[hashAlgorithm.GetDigestSize()];
+            hashAlgorithm.BlockUpdate(data, 0, data.Length);
+            hashAlgorithm.DoFinal(hashedData, 0);
 
-            return symmetricKeyAlgorithm.CreateSymmetricKey(keyMaterial);
+            return hashedData;
         }
 
         private static ICipherParameters GenerateCipherKey(byte[] key)
@@ -214,6 +339,15 @@
 
             cipher.Init(encrypt, key);
             return cipher.DoFinal(blockRoundedData);
+        }
+
+        private static bool VerifySIgnature(byte[] data, byte[] signature, X509Certificate certificate)
+        {
+            ISigner signer = SignerUtilities.GetSigner("SHA256withRSA");
+            signer.Init(false, certificate.GetPublicKey());
+            signer.BlockUpdate(data, 0, data.Length);
+
+            return signer.VerifySignature(signature);
         }
 
         private byte[] GenerateRandomBytes(int length)
